9e8cc2ddde27209622d5eabb495c3eba3fc45a22
charnon.ng <charnonng1997@gmail.com>
2020-04-06 20:34:01 +1200

    app now takes custom user response

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class TodoApp extends StatelessWidget {
 class TodoList extends StatefulWidget{
 
   @override
-  createState() => new TodoListState();
+  State<StatefulWidget> createState() => TodoListState();
 
 }
 
@@ -27,11 +27,14 @@ class TodoListState extends State<TodoList> {
 
   List<String> _todoItems = [];
 
-  void _addTodoItem(){
-    setState((){
-      int index = _todoItems.length;
-      _todoItems.add("Item " + index.toString());
-    });
+  void _addTodoItem(String task){
+
+    if(task.length > 0 ){
+      setState(() {
+        _todoItems.add(task);
+      });
+    }
+    
   }
 
   Widget _buildTodoList(){
@@ -58,9 +61,34 @@ class TodoListState extends State<TodoList> {
       ),
       body: _buildTodoList(),
       floatingActionButton: FloatingActionButton(
-        onPressed: _addTodoItem,
+        onPressed: _pushAddTodoScreen,
         tooltip: 'Add task',
         child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _pushAddTodoScreen(){
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context){
+          return new Scaffold(
+            appBar: AppBar(
+              title: Text("Add a new task"),
+            ),
+            body: TextField(
+              autofocus: true,
+              onSubmitted: (val){
+                _addTodoItem(val);
+                Navigator.pop(context);
+              },
+              decoration: new InputDecoration(
+                hintText: "Enter something to do...",
+                contentPadding: const EdgeInsets.all(16.0)
+              ),
+            ),
+          );
+        }
       ),
     );
   }
